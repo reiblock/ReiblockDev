443b979ddbcee7e507a44086f6d314a4d5e00fd8
Vihar Chokshi <vc@iot4.net>
2022-03-01 16:04:42 -0700

    Add terraform backend bucket

---
+++ terraform/prerequisites/main.tf
@@ -0,0 +1,24 @@
+# Entry point into the infrastructure
+# We create one bucket (file system) in AWS S3 as a stored folder
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+# Create an s3 bucket that we can use for the backend
+
+resource "aws_s3_bucket" "backend" {
+  bucket = "reiblock-iot4-terraform-state-backend"
+  tags = {
+    Name = "Terraform State Files"
+  }
+}
