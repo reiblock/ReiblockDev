4bafe447b1ae1d1a8c3710f8d3ed15ddb2af3254
Matthias Schreiber <matthias.schreiber@zoho.com>
2022-03-13 17:06:40 -0300

    Added hello world main.py and setup.py

---
+++ python/hello_world/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ReiblockDev',
+    version='1.0',
+    packages=['src'],
+    url='',
+    license='',
+    author='matthias',
+    author_email='mat@pyfection.com',
+    description='Packaging of hello world'
+)
