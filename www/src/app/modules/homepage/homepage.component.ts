648999e6d2dc7e33c141c52187bb55d3a162429f
Gaurav Kunal <gaurav.kunal@softobotics.com>
2022-06-07 13:42:04 +0530

    client mentioned ui changes is done

--- www/src/app/modules/homepage/homepage.component.ts
+++ www/src/app/modules/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TestService } from 'src/app/services/test.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,13 +8,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent implements OnInit {
-  public emailForm!: FormGroup;
+  public zipForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.emailForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+  constructor(private fb: FormBuilder,private testService: TestService) {
+    this.zipForm = this.fb.group({
+      zipCode: ['', [Validators.required,Validators.minLength(5)]],
     });
   }
 
   ngOnInit(): void {}
+
+  onSubmit(){
+    
+this.testService.setZipCode(this.zipForm.value.zipCode);
+  }
 }
+
+
